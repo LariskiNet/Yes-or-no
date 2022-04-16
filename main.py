625ef8f41b7aed5ac180a63167e5ac8472a72fee
oktoberf <75834827+oktoberf@users.noreply.github.com>
2022-04-16 23:23:23 +0300

    test

--- main.py
+++ main.py
@@ -10,5 +10,10 @@ dp = Dispatcher()
 
 
 @dp.request_handler()
-def handle_dialog(request, response, user_storage):
+def handle_dialog(request, response, user_storage, alice_request):
     return alice_request.response('Привет этому миру!')
+
+
+if __name__ == '__main__':
+    app = get_new_configured_app(dispatcher=dp, path=WEBHOOK_URL_PATH)
+    web.run_app(app, host=WEBAPP_HOST, port=WEBAPP_PORT)
