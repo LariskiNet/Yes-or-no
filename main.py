76bd7a79f6daf36d7c5c3b0769e3f64968e91dab
oktoberf <75834827+oktoberf@users.noreply.github.com>
2022-04-16 23:21:50 +0300

    test

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from alice_blue import *
-import logging
-logging.basicConfig(level=logging.DEBUG)
-access_token = AliceBlue.login_and_get_access_token(
-    username='username', password='password', twoFA='1860', api_secret='api_secret', app_id='app_id')
-alice = AliceBlue(username='username', password='password',
-                  access_token=access_token)
-print(alice.get_balance())  # get balance / margin limits
-print(alice.get_profile())  # get profile
-print(alice.get_daywise_positions())  # get daywise positions
-print(alice.get_netwise_positions())  # get netwise positions
-print(alice.get_holding_positions())  # get holding positions
+from aiohttp import web
+from aioalice import Dispatcher, get_new_configured_app
+
+WEBHOOK_URL_PATH = 'https://github.com/LariskiNet/Yes-or-no.git'  # webhook endpoint
+
+WEBAPP_HOST = 'localhost'
+WEBAPP_PORT = 3001
+
+dp = Dispatcher()
+
+
+@dp.request_handler()
+def handle_dialog(request, response, user_storage):
+    return alice_request.response('Привет этому миру!')
