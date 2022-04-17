ce25fc22b93fa5e9caf4d5d224b1af52a382f2b0
oktoberf <75834827+oktoberf@users.noreply.github.com>
2022-04-17 12:28:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,8 @@ logging.basicConfig(level=logging.DEBUG)
 
 @app.route("/", methods=['POST'])
 def main():
-    logging.info(request.json)
+    return '<h1>flask app<h1>'
+
+
+if __name__ == '__main__':
+    app.run()
