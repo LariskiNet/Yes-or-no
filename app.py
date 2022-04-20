ba2be6bf56d1145c76e6d3557873ed297a11e413
oktoberf <75834827+oktoberf@users.noreply.github.com>
2022-04-20 21:23:22 +0300

    test

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from flask import Flask, request
+from aioalice import *
 import logging
 
 app = Flask(__name__)
 
 logging.basicConfig(level=logging.DEBUG)
 
+dp = Dispatcher()
+
+
+@app.route('/')
+def index():
+    return 'Flask is running!'
+
 
 @app.route("/", methods=['POST'])
 def main():
     logging.info(request.json)
+
+
+@dp.request_handler()
+async def handle_all_requests(alice_request):
+    return alice_request.response('Привет этому миру!')
+
+if __name__ == "__main__":
+    app.run(ssl_context=('cert.pem', 'key.pem'))
