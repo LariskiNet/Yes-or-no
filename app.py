9575cd08329b5d3b4124fae46cc130b6db3fdc44
LariskiNet <lariski22@yandex.ru>
2022-04-21 01:50:55 +0300

    master

--- app.py
+++ app.py
@@ -1,27 +1,34 @@
 from flask import Flask, request
 from aioalice import *
 import logging
-
+from dialogic.dialog_connector import DialogConnector
+from dialogic.dialog_manager import TurnDialogManager
+from dialogic.server.flask_server import FlaskServer
+from dialogic.cascade import DialogTurn, Cascade
+csc = Cascade()
 app = Flask(__name__)
-
-logging.basicConfig(level=logging.DEBUG)
-
 dp = Dispatcher()
-
-
-@app.route('/')
-def index():
-    return 'Flask is running!'
-
+logging.basicConfig(level=logging.DEBUG)
 
 @app.route("/", methods=['POST'])
 def main():
     logging.info(request.json)
 
 
-@dp.request_handler()
-async def handle_all_requests(alice_request):
-    return alice_request.response('Привет этому миру!')
+@csc.add_handler(priority=10, regexp='(hello|hi|привет|здравствуй)')
+def hello(turn: DialogTurn):
+    turn.response_text = 'Привет! Это единственная условная ветка диалога.'
+
+
+@csc.add_handler(priority=1)
+def fallback(turn: DialogTurn):
+    turn.response_text = 'Я вас не понял. Скажите мне "Привет"!'
+    turn.suggests.append('привет')
+
+dm = TurnDialogManager(cascade=csc)
+connector = DialogConnector(dialog_manager=dm)
+server = FlaskServer(connector=connector)
+
 
 if __name__ == "__main__":
-    app.run(ssl_context=('cert.pem', 'key.pem'))
+    app.run(ssl_context='adhoc')
